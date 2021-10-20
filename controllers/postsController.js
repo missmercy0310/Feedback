60a40f8b850c70e6a4f02ba65efe5716c39d7c69
ElliotHarrison <ellioth1029@gmail.com>
2021-10-19 22:56:42 -0700

    adds create route, delete route, and update to posts to postController <-- also renamed

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -9,7 +9,7 @@ router.get("/", async function (req, res) {
     try {
         const allPosts = await Post.find({});
         const context = {
-            posts: allPosts
+            posts: allPosts,
         };
         return res.render("posts/index", context);
     } catch (error) {
@@ -31,11 +31,72 @@ router.get("/:id", function (req, res, next) {
             }
             const context = {
                 post,
-                comments: foundComments,
+                comments: foundComments
             };
             return res.render("posts/show", context);
         });
     });
 });
+
+// Create
+router.post("/", async function (req, res, next) {
+  
+    try { // body == data incoming with a request
+    
+      await Post.create(data);
+      console.log(newPost);
+      return res.redirect("/posts");
+    } catch (error){
+      console.log(error);
+      req.error = error;
+      return next();
+    }
+});
+
+//DELETE POST 
+
+router.delete("/:id", function (req, res, next) {
+    Product.findByIdAndDelete(req.params.id, function (error, deletedPost) {
+      if (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+      }
+  
+      //DELETE COMMENTS OF EACH POST 
+      Comment.deleteMany(
+        { post: req.params.id },
+        function (error, deletedComments) {
+          if (error) {
+            console.log(error);
+            req.error = error;
+            return next();
+          }
+          return res.redirect("/posts");
+        }
+      );
+    });
+  });
+  
+  //UPDATE TO POST---    ADD UPDATE TO COMMENT 
+
+  router.put("/:id", function (req, res, next) {
+    Post.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+      function (error, updatedPost) {
+        if (error) {
+          console.log(error);
+          req.error = error;
+          return next();
+        }
+  
+        res.redirect(`/posts/${req.params.id}`);
+      }
+    );
+  });
+
+
 
 module.exports = router;
